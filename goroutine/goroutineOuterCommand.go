290370327a8feb88fa28efeb507873aec5dd8418
tom--bo <otsuka.t.2013@gmail.com>
2016-10-25 22:11:11 +0900

    goroutine wait

--- goroutine/goroutineOuterCommand.go
+++ goroutine/goroutineOuterCommand.go
@@ -5,21 +5,26 @@ import (
     "os/exec"
     "time"
     "runtime"
+    "sync"
 )
 
 func main() {
-    runtime.GOMAXPROCS(runtime.NumCPU())
-	fmt.Println(runtime.NumCPU())
+    runtime.GOMAXPROCS(1)
+	// fmt.Println(runtime.NumCPU())
     start := time.Now()
-    go outerCommand()
-    go outerCommand()
-    go outerCommand()
-    go outerCommand()
-    go outerCommand()
+    wg := &sync.WaitGroup{}
+    c := 300
+
+    wg.Add(c)
+    for i:=0; i<c; i++ {
+        go outerCommand(wg)
+    }
+	wg.Wait()
     end := time.Now()
     fmt.Printf("%f秒\n", (end.Sub(start)).Seconds())
 }
 
-func outerCommand() {
+func outerCommand(wg *sync.WaitGroup) {
     exec.Command("sleep", "1").Run()
+    wg.Done()
 }
